14ed8a8924a0eb3c9fe2b8a2f42b64c4e57243a2
Nazmul Hossain Pappu <mdpappu006@gmail.com>
2020-11-20 20:38:27 +0600

    Update SQL
    added become a donor table

--- SQL-DB/organ.sql
+++ SQL-DB/organ.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 18, 2020 at 05:10 PM
+-- Generation Time: Nov 20, 2020 at 02:37 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.4.0
 
@@ -43,6 +43,64 @@ CREATE TABLE IF NOT EXISTS `admin` (
 
 INSERT INTO `admin` (`id`, `email`, `password`) VALUES
 (1, 'admin@gmail.com', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `become_donor`
+--
+
+DROP TABLE IF EXISTS `become_donor`;
+CREATE TABLE IF NOT EXISTS `become_donor` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `age` int(3) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` int(15) NOT NULL,
+  `adress` varchar(255) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `gender` varchar(30) NOT NULL,
+  `e_name` int(100) NOT NULL,
+  `e_relationship` int(100) NOT NULL,
+  `e_phone` int(15) NOT NULL,
+  `e_email` varchar(100) NOT NULL,
+  `e_address` varchar(255) NOT NULL,
+  `blood_group` varchar(30) NOT NULL,
+  `blood_donate` varchar(100) NOT NULL,
+  `organs_donate` varchar(100) NOT NULL,
+  `body_part` varchar(100) NOT NULL,
+  `living_donate` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_request`
+--
+
+DROP TABLE IF EXISTS `post_request`;
+CREATE TABLE IF NOT EXISTS `post_request` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `patientName` varchar(100) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` int(30) NOT NULL,
+  `requiredorgan` varchar(100) NOT NULL,
+  `prescription` varchar(255) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `donate_user` varchar(100) NOT NULL,
+  `action` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `post_request`
+--
+
+INSERT INTO `post_request` (`id`, `patientName`, `address`, `phone`, `requiredorgan`, `prescription`, `email`, `donate_user`, `action`) VALUES
+(1, 'User', 'Tangail', 1840488752, 'Blood', '', 'user@gmail.com', 'blood', 1);
 
 -- --------------------------------------------------------
 
