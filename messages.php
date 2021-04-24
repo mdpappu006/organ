8993a41c866e965c248a5e881b3dc601718c7a16
Nazmul Hossain Pappu <mdpappu006@gmail.com>
2021-04-24 10:23:36 +0600

    messages=> new Update UI.

--- messages.php
+++ messages.php
@@ -11,80 +11,44 @@
 
     <div class="container">
         <div class="row">
+
+        <div class="col-md-12">
+            <h4 class="mt-5 text-center">Messages</h4>
+        </div>
+
             <div class="col-md-12">
                 <form action="messages.php" method="POST" class="chat-area">
                     <div class="chat-main">
                         <div class="admin-chat">
                             <div class="profile-left">
                                 <span class="admin-profile"><b>A</b></span>
+                                <h6>Admin</h6>
                             </div>
 
                             <div class="profile-info">
                                 <div class="d-inline-block">
-                                    <h6>Admin</h6>
-                                    <p> Hi, How are you?</p>
+                                    <p class="admin-u"> Hi, How are you?</p>
                                 </div>
                             </div>
                         </div>
                         
                         <div class="admin-chat user">
-                            <div class="profile-left">
-                                <span class="admin-profile"><b>NH</b></span>
-                            </div>
 
                             <div class="profile-info ">
                                 <div class="d-inline-block">
-                                    <h6>Nazmul Hossain</h6>
-                                    <p> Yeah, I am great. how about you?</p>
+                                    <p class="current-user"> Yeah, I am great. how about you?</p>
                                 </div>
                             </div>
                         </div>
+
                         
-                        <div class="admin-chat user">
-                            <div class="profile-left">
-                                <span class="admin-profile"><b>NH</b></span>
-                            </div>
-
-                            <div class="profile-info ">
-                                <div class="d-inline-block">
-                                    <h6>Nazmul Hossain</h6>
-                                    <p> Yeah, I am great. how about you?</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="admin-chat">
-                            <div class="profile-left">
-                                <span class="admin-profile"><b>A</b></span>
-                            </div>
-
-                            <div class="profile-info">
-                                <div class="d-inline-block">
-                                    <h6>Admin</h6>
-                                    <p> Hi, How are you?</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="admin-chat">
-                            <div class="profile-left">
-                                <span class="admin-profile"><b>A</b></span>
-                            </div>
-
-                            <div class="profile-info">
-                                <div class="d-inline-block">
-                                    <h6>Admin</h6>
-                                    <p> Hi, How are you?</p>
-                                </div>
-                            </div>
-                        </div>
-
+                      
                     </div>
 
                     <div class="submit-user-chat">
                         <div class="chat-box">
-                            <textarea name="" rows="3"></textarea>
-                            <button class="btn btn-primary btn-chats">Send</button>
+                            <textarea name="" rows="3" id="chat-text"></textarea>
+                            <button id="send" class="btn btn-primary btn-chats">Send</button>
                         </div>
                     </div>
                 </form>
@@ -96,3 +60,8 @@
 <!--footers-->
 <?php include_once('header/footer.php') ;?>
 <!--footers-->
+
+
+<script>
+    $(".chat-main").scrollTop($(".chat-main").prop('scrollHeight'));
+</script>
